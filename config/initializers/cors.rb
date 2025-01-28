71f5bf0511b7db43d768f85f31166b55d816657e
Manuel Lipo <lipostylee@gmail.com>
2025-01-28 13:37:07 +0200

    initializing project

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Specify your frontend origins explicitly
-    # origins 'http://localhost:3001', 'http://192.168.1.5:3001'
-    origins 'https://www.e-nutritionist.coom', 'https://www.otimasvrei.g'
+    origins 'https://www.e-nutritionist.com', 'https://www.otimasvrei.gr', 'http://192.168.1.5:3001', 'http://localhost:3001'
 
-    # Allow all resource types with specific methods
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
-             credentials: true # Allow credentials if needed
+             credentials: true
   end
 end
